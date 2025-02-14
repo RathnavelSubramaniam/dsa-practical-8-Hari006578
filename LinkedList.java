6348790394b1897ee63499b4eaa91e7bc7280cde
Hari006578 <krishnahari45688@gmail.com>
2025-02-14 04:16:05 +0000

    SUBMIT

--- LinkedList.java
+++ LinkedList.java
@@ -60,12 +60,26 @@ int data,data1;
             list.displayList();
           }
         }
-    }
-      public void insertEnd(int data) 
-    {    
-       //TYPE YOUR CODE HERE 
-    }  
-     
+    }       
+     public void insertEnd(int data)
+{
+Node newNode = new Node(data);
+if(head == null)
+{
+head = newNode;
+tail = newNode;
+}
+else
+{
+Node temp=head;
+while( temp.next!= null)
+{
+temp = temp.next;
+}
+temp.next=newNode;
+System.out.println("ELEMENT INSERTED");
+}
+} 
 public void displayList() {
         Node current = head;
         while (current != null) {
